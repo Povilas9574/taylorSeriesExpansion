eef3b09fc6ebaaf20bc28e80b3e62ccb69de5b00
Povilas <povilas.zvirblis1@gmail.com>
2016-12-03 16:33:27 +0200

    Packages and interfaces added

---
+++ TaylorSeries/TaylorSeries/MathSAF/MathSAF.TaylorSeriesExpansionsAPI/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaylorSeries.MathSAF.MathSAF.TaylorSeriesExpansionsAPI
+{
+    class Result
+    {
+    }
+}
