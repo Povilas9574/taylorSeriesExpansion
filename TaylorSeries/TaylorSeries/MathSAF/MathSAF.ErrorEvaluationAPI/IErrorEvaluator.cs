b9e8a70a246b1582c22fd9612ac8bb0e425acd92
Povilas <povilas.zvirblis1@gmail.com>
2016-12-03 16:40:04 +0200

    Evaluator interface add

---
+++ TaylorSeries/TaylorSeries/MathSAF/MathSAF.ErrorEvaluationAPI/IErrorEvaluator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaylorSeries.MathSAF.MathSAF.ErrorEvaluationAPI
+{
+    interface IErrorEvaluator
+    {
+        int Evaluate(double m, double error, double x);
+    }
+}
