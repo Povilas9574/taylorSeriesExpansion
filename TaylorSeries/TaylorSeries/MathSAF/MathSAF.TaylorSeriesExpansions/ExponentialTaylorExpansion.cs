52db759c7161176d1d6d82fdde8822d7e2dafaa0
Povilas <povilas.zvirblis1@gmail.com>
2017-01-14 01:15:00 +0200

    ExponentialTaylorSeriesExpansion add

---
+++ TaylorSeries/TaylorSeries/MathSAF/MathSAF.TaylorSeriesExpansions/ExponentialTaylorExpansion.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaylorSeries.MathSAF.MathSAF.ErrorEvaluationAPI;
+using TaylorSeries.MathSAF.MathSAF.TaylorSeriesExpansionsAPI;
+using TaylorSeries.MathSAF.MathSAF.Helpers;
+
+namespace TaylorSeries.MathSAF.MathSAF.TaylorSeriesExpansions
+{
+    class ExponentialTaylorExpansion : ITaylorExpansion
+    {
+        private double error;
+        private double pow = 1;
+        private IErrorEvaluator evaluator;
+        private FactorialHelper factorial = new FactorialHelper();
+
+        public ExponentialTaylorExpansion(double error, int pow)
+        {
+            this.error = error;
+            this.pow = pow;
+        }
+
+        public ExponentialTaylorExpansion(double error)
+        {
+            this.error = error;
+        }
+
+        public Result Calculate()
+        {
+            double expBase;
+            double m;
+            Result result = new Result();
+            int n;
+            double res = 0;
+
+            if(pow >= 1)
+            {
+                expBase = 3;
+            }
+            else if(pow < 0)
+            {
+                expBase = 2.5;
+            } 
+            else
+            {
+                result.Answer = 1;
+                return result;
+            }
+
+            m = Math.Pow(expBase, pow);
+            n = evaluator.Evaluate(m, error, pow);
+
+            for (int i = 0; i <= n; i++)
+            {
+                res += (Math.Pow(pow, i)/factorial.CalculateFactNoRecursion(i));
+            }
+
+            result.Answer = res;
+            return result;
+        }
+        
+
+    }
+}
