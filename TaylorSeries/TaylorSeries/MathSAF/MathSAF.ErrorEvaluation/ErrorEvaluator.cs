696ff54f2ccd8415f709e8ec6b61ef0b650f864b
Tautvydas Versockas <Tautvydas Versockas>
2017-01-14 14:39:41 +0200

    r Please enter the commit message for your changes. Lines starting r evaluator

---
+++ TaylorSeries/TaylorSeries/MathSAF/MathSAF.ErrorEvaluation/ErrorEvaluator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaylorSeries.MathSAF.MathSAF.ErrorEvaluationAPI;
+
+namespace TaylorSeries.MathSAF.MathSAF.ErrorEvaluation
+{
+    public class ErrorEvaluator : IErrorEvaluator
+    {
+        public int Evaluate(double m, double error, double x)
+        {
+            Helpers.FactorialHelper factorial = new Helpers.FactorialHelper();
+            int n=0;
+            while (true)
+            {
+                if ((m * Math.Pow(Math.Abs(x), n + 1)) / factorial.CalculateFactorial(n + 1) >= error)
+                {
+                    return n;
+                }
+            }
+        }
+    }
+}
